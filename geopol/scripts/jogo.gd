60b300d34c04c748f6962556d5ed27f9a7d3554a
RoGeRio "rgrcnh" CuNHa <rogerio.cunha@gmail.com>
2016-12-13 14:43:59 -0200

    Add files via upload

---
+++ geopol/scripts/jogo.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
+func on_b_pergunta_pressed(obj,pai):
+	print("Pressed " + str(obj)+ str(pai))
+
+
+func on_b_resposta_pressed(obj,pai):
+	print("Pressed " + str(obj)+ str(pai))
+	pass # replace with function body
---
+++ geopol/scripts/pergunta.gd
@@ -0,0 +1,9 @@
+extends TextureButton
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	connect("pressed",get_node("/root/jogo"),"on_b_pergunta_pressed",[get_name(), get_parent().get_name()])
+	pass
