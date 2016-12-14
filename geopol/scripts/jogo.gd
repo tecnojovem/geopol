533e12ea67f948371269cc051e4f0105e40c0db7
Rogerio Ferreira da Cunha <roger@petrobras.com.br>
2016-12-14 12:03:41 -0200

    mudancas para carregar o arquivo de perguntas e respostas

--- geopol/scripts/jogo.gd
+++ geopol/scripts/jogo.gd
@@ -1,21 +1,41 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+const qa_file = "user://questions.json"
+var qa={} 
 
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+
+func load_game():
+	var file = File.new()
+	if !file.file_exists(qa_file):
+		return
+	file.open(qa_file, File.READ)
+	if file.is_open():
+		var tot_file=""
+		while (!file.eof_reached()):
+			tot_file += file.get_line()
+		qa.parse_json(tot_file)
+		print("linha" + str(qa))
+		file.close()
 	pass
 
+func _ready():
+	load_game()
+	pass
+	
+
+
+func save_game():
+	var file = File.new()
+	if !file.file_exists(qa_file):
+		print ("Sera novo arquivo")
+		file.open(qa_file, File.WRITE)
+		file.store_line(qa.to_json())
+		print("QA Saved!")
 
 func on_b_pergunta_pressed(obj,pai):
-	print("Pressed " + str(obj.right(5))+ \
-	" Pai: " + str(pai))
+	print("Pressed " + str(obj)+ str(pai))
 
 
 func on_b_resposta_pressed(obj,pai):
-	print("Pressed " + str(obj).right(5) + \
-	" Pai:" + str(pai))
+	print("Pressed " + str(obj)+ str(pai))
 	pass # replace with function body
